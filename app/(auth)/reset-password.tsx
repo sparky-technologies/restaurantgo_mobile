33a05607c66b66ac72220b83e2be90e081c7f186
Ayobami Alaran A. <ayobamidele006@gmail.com>
2024-08-21 22:27:34 +0100

    implement reset password screen

--- app/(auth)/reset-password.tsx
+++ app/(auth)/reset-password.tsx
@@ -1,14 +1,53 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import InputField from "@/components/InputField";
+import CustomButton from "@/components/CustomButton";
 
-type Props = {}
+type Props = {};
 
 const ResetPassword = (props: Props) => {
-  return (
-    <View>
-      <Text>ResetPassword</Text>
-    </View>
-  )
-}
+  const [form, setForm] = useState({
+    email: "",
+  });
 
-export default ResetPassword
+  const handleResetButtonPress = () => {
+    // TODO: api request reset password
+    console.log("Reset Password");
+    console.log(form);
+  };
+  const [loading, setLoading] = useState(false);
+  const [secureTextEntry, setSecureTextEntry] = useState(false);
+  return (
+    <SafeAreaView className="p-6">
+      <View className="flex justify-start flex-col">
+        <Text className="text-[24px] font-StratosBold">Reset Password</Text>
+        <Text className="font-StratosLight text-[13px]">
+          It's your identity, protect it
+        </Text>
+      </View>
+      <View className="mt-4">
+        <InputField
+          label={"Email"}
+          placeholder="ayobami@dev.com"
+          value={form.email}
+          onChangeText={(email) =>
+            setForm({
+              ...form,
+              email,
+            })
+          }
+        />
+      </View>
+      <View className="mt-4">
+        <CustomButton
+          loading={loading}
+          title="Reset Password"
+          handlePress={handleResetButtonPress}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default ResetPassword;
--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,15 +1,85 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import InputField from "@/components/InputField";
+import { icons } from "@/constants";
+import { Link, router } from "expo-router";
+import CustomButton from "@/components/CustomButton";
 
-type Props = {}
+type Props = {};
 
 const SignIn = (props: Props) => {
+  const [loading, setLoading] = useState(false);
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+  const handleLogin = () => {
+    console.log("Login Form submitted");
+    console.log(form);
+    router.push("/(root)/(tabs)/home");
+  };
+  const [secureTextEntry, setSecureTextEntry] = useState(false);
   return (
-    <SafeAreaView>
-      <Text>SignIn</Text>
+    <SafeAreaView className="p-6">
+      <View className="flex justify-start">
+        <Text className="font-StratosBold text-[24px]">
+          Hey Cheif! {"\n"}
+          Good to see you again
+        </Text>
+      </View>
+      <View className="mt-2">
+        <InputField
+          label={"Email"}
+          placeholder="ayobami@yahoo.com"
+          value={form.email}
+          onChangeText={(email) =>
+            setForm({
+              ...form,
+              email,
+            })
+          }
+        />
+        <InputField
+          label={"Password"}
+          placeholder="somepassword"
+          secureTextEntry={secureTextEntry}
+          setSecureTextEntry={setSecureTextEntry}
+          icon={secureTextEntry ? icons.eyes : icons.eyeOpen}
+          value={form.password}
+          onChangeText={(password: string) =>
+            setForm({
+              ...form,
+              password,
+            })
+          }
+        />
+        <View className="flex flex-row justify-end">
+          <Text className="font-Stratos">Forgot Password </Text>
+          <Link href={"/(auth)/reset-password"}>
+            <Text className="text-primary underline font-Stratos">
+              Reset here
+            </Text>
+          </Link>
+        </View>
+        <View className="mt-5">
+          <CustomButton
+            loading={loading}
+            title="Login"
+            handlePress={handleLogin}
+          />
+          <View className="flex mt-2 flex-row justify-center">
+            <Text className="font-Stratos">Don't have an account?, </Text>
+            <Link href="/(auth)/sign-up">
+              <Text className="text-primary underline font-Stratos">
+                register
+              </Text>
+            </Link>
+          </View>
+        </View>
+      </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
