7c0865cc13283ff4d557f699d6fbc1ba44db9036
Ayobami Alaran A. <ayobamidele006@gmail.com>
2024-09-03 22:26:18 +0100

    feat: Implement address saving functionality
    Adds a new form for users to input and save their addresses. This includes selecting a state and city, as well as providing a detailed street address. Upon saving, a confirmation modal is displayed, enhancing the user experience.

--- app/address/add.tsx
+++ app/address/add.tsx
@@ -1,13 +1,111 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, TextInput } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Nav from "@/components/Nav";
+import { router, Stack } from "expo-router";
+import { Picker } from "@react-native-picker/picker";
+import CustomButton from "@/components/CustomButton";
+import CustomModal from "@/components/CustomModal";
 
 type Props = {};
 
 const Add = (props: Props) => {
+  const [form, setForm] = useState({
+    state: "",
+    city: "",
+    address: "",
+  });
+  const handleDone = () => {
+    router.back();
+  };
+  const [showModal, setShowModal] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const handleSaveAddress = () => {
+    setLoading(true);
+    console.log("Address saved");
+    console.log(form);
+    setShowModal(true);
+    setLoading(false);
+  };
   return (
-    <View>
-      <Text>Add</Text>
-    </View>
+    <SafeAreaView>
+      <Stack.Screen
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Nav title="Add New Address" />
+
+      <View className="p-6">
+        <View className="my-4">
+          <Text className="text-[16px] font-StratosSemiBold">
+            Select your state
+          </Text>
+          <View className="rounded-[8px] my-3 border-[1px] border-primary">
+            <Picker
+              selectedValue={form.state}
+              className="w-full h-[46px]"
+              onValueChange={(itemValue, itemIndex) => {
+                setForm({
+                  ...form,
+                  state: itemValue,
+                });
+              }}
+            >
+              <Picker.Item label="Lagos" value="Lagos" />
+            </Picker>
+          </View>
+        </View>
+        <View className="my-2">
+          <Text className="text-[16px] font-StratosSemiBold">
+            Select your city
+          </Text>
+          <View className="rounded-[8px] my-3 border-[1px] border-primary">
+            <Picker
+              selectedValue={form.city}
+              className="w-full h-[46px]"
+              onValueChange={(itemValue, itemIndex) => {
+                setForm({
+                  ...form,
+                  city: itemValue,
+                });
+              }}
+            >
+              <Picker.Item label="Island" value="Island" />
+            </Picker>
+          </View>
+        </View>
+        <View>
+          <Text className="text-[16px] font-StratosSemiBold">
+            Enter your address
+          </Text>
+          <View className="rounded-[8px] my-3 border-[1px] border-primary">
+            <TextInput
+              numberOfLines={4}
+              value={form.address}
+              onChangeText={(address) => setForm({ ...form, address })}
+              placeholder="123 Main St"
+              className="w-full px-2 h-[100px]"
+            />
+          </View>
+        </View>
+
+        <View className="mt-10">
+          <CustomButton
+            title="Save"
+            loading={loading}
+            handlePress={handleSaveAddress}
+          />
+        </View>
+        <CustomModal
+          btnTitle="Continue"
+          showModal={showModal}
+          handleDone={handleDone}
+          text="Address saved successfully"
+          title="Success"
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
