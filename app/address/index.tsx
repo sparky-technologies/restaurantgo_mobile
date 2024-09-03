46b2b6c2907fce96ab693ea15e41a62c8c7f14a7
Ayobami Alaran A. <ayobamidele006@gmail.com>
2024-09-03 15:36:57 +0100

    feat: Implement address listing and navigation
    This commit introduces a new feature allowing users to view and manage their saved addresses.
    
    Users can now view a list of their addresses, each displayed with edit and delete functionality.  Additionally, a button for adding new addresses is provided, enhancing the user experience by streamlining address management within the application.

--- app/address/index.tsx
+++ app/address/index.tsx
@@ -1,12 +1,67 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Nav from "@/components/Nav";
-import { Stack } from "expo-router";
+import { router, Stack } from "expo-router";
+import Feather from "@expo/vector-icons/Feather";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import CustomButton from "@/components/CustomButton";
 
 type Props = {};
 
 const Address = (props: Props) => {
+  const addresses = [
+    {
+      id: 1,
+      name: "Home",
+      address: "123 Main St, City, State, ZIP",
+      state: "New York",
+      city: "Brookln",
+    },
+    {
+      id: 2,
+      name: "Work",
+      address: "456 Park Ave, City, State, ZIP",
+      state: "California",
+      city: "San Francisco",
+    },
+    {
+      id: 3,
+      name: "School",
+      address: "789 Oak St, City, State, ZIP",
+      state: "Texas",
+      city: "Houston",
+    },
+  ];
+  const handleEdit = (id: any) => {
+    router.push(`/address/${id}`);
+  };
+
+  const handleDelete = (id: any) => {
+    console.log("Deleting");
+  };
+  const AddressCard = ({ item }: any) => (
+    <View className="w-[311px] my-3 relative p-4 h-[122px] bg-white shadow-md">
+      <View className="flex flex-col">
+        <Text className="text-[20px] mb-2 font-StratosMedium">
+          {item.address}
+        </Text>
+        <Text className="text-[15px] font-StratosLight">
+          {item.city}-{item.state}
+        </Text>
+      </View>
+      <View className="flex mb-3 mx-3 absolute right-0 bottom-0 flex-row justify-end items-center">
+        <View className="flex flex-row gap-2">
+          <TouchableOpacity onPress={() => handleDelete(item.id)}>
+            <Feather name="trash" size={24} color="red" />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => handleEdit(item.id)}>
+            <FontAwesome name="pencil" size={24} color="red" />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
   return (
     <SafeAreaView>
       <Stack.Screen
@@ -15,6 +70,18 @@ const Address = (props: Props) => {
         }}
       />
       <Nav title="Address" />
+      <View className="p-6">
+        {addresses.map((item) => (
+          <AddressCard key={item.id} item={item} />
+        ))}
+        <View className="mt-10">
+          <CustomButton
+            loading={false}
+            title="Add New Address"
+            handlePress={() => router.push("/address/add")}
+          />
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
